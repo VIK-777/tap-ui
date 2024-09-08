c3d143d56b2735a8f4dd77a5498125dde781baac
VIK-777 <36794502+VIK-777@users.noreply.github.com>
2024-09-08 21:31:26 +0200

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Home() {
-  const pathname = usePathname()
-  const basePathSegments = pathname.split('/').slice(0, -1)
-  const basePath = basePathSegments.join('/')
+  // const pathname = usePathname()
+  // const basePathSegments = pathname.split('/').slice(0, -1)
+  // const basePath = basePathSegments.join('/')
+
+  // Get the current URL origin (e.g., "https://mysite.com")
+  const origin = typeof window !== 'undefined' ? window.location.origin : '';
+
+  // Get the pathname without the leading slash
+  const pathname = typeof window !== 'undefined' ? window.location.pathname.replace(/^\//, '') : '';
+  console.log("pathname: " + pathname)
+
+  // Construct the base path
+  const basePath = pathname ? `/${pathname}` : '';
+
   console.log("Base path: " + basePath)
   return (
     <body>
-      <div><a href={`${basePath}/farms`}>Farms</a></div>
-      <div><a href={`${basePath}/userAssets`}>User Assets</a></div>
+      <div><Link href={`${basePath}/farms`}>Farms</Link></div>
+      <div><Link href={`${basePath}/userAssets`}>User Assets</Link></div>
     </body>
   )
 }
