eb79a2d29ed107481f9d2e047ce1c30bb6fc76c4
VIK-777 <36794502+VIK-777@users.noreply.github.com>
2024-09-08 21:21:18 +0200

    Fix base path on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+
 export default function Home() {
+  const pathname = usePathname()
+  const basePathSegments = pathname.split('/').slice(0, -1)
+  const basePath = basePathSegments.join('/')
   return (
     <body>
-      <div><a href="./farms">Farms</a></div>
-      <div><a href="./userAssets">User Assets</a></div>
+      <div><a href={`${basePath}/farms`}>Farms</a></div>
+      <div><a href={`${basePath}/userAssets`}>User Assets</a></div>
     </body>
   )
 }
