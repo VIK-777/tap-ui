9bf109c7f70817efa4711383d0440f07e0ece155
VIK-777 <36794502+VIK-777@users.noreply.github.com>
2024-09-08 21:03:59 +0200

    Change links to child pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <body>
-      <div><a href="/farms">Farms</a></div>
-      <div><a href="/userAssets">User Assets</a></div>
+      <div><a href="./farms">Farms</a></div>
+      <div><a href="./userAssets">User Assets</a></div>
     </body>
   )
 }
