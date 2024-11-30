46650b80984484960535ffb79ad5d5b63eea6021
VIK-777 <36794502+VIK-777@users.noreply.github.com>
2024-11-30 22:39:25 +0100

    Change site name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Vanguard Vision on TON",
-  description: "Vanguard Vision on TON",
+  title: "Vanguard Vision",
+  description: "Vanguard Vision",
 }
 
 export default function RootLayout({
